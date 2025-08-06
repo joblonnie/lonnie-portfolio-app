fe4f165f4583578c28d15bc769e692aa3560975a
joblonnie <joblonnie@gmail.com>
2025-08-07 01:49:58 +0900

    fix: :bug: 다크 모드 에러 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,9 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+            enableSystem={true}
+            disableTransitionOnChange={false}
+            storageKey="portfolio-theme"
           >
             <LanguageProvider>
               {children}
